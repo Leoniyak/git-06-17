429edcb7a8767b6f8237c3a4cec2d559bcbb624d
Leoniyak <iyakareleon@gmail.com>
2022-06-18 05:43:48 +0000

    My first commit

---
+++ sample.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#the following scripts are about git
+
+echo " Hello, what is your name ?"
+
+read PERSON
+
+echo "Hello, $PERSON"
+
+echo "How old are you, $PERSON ?"
+
+read AGE
+
+echo " $PEARSON , You are too old, go to sleep"
+
+
